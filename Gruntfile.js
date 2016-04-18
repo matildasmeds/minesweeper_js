c8ba62aae2c26a52db76df9721c20679704cea22
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-04-18 23:38:31 +0300

    Using Karma to run Mocha and Chai tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,37 @@ module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // Mocha
-    mocha: {
-      all: {
-        src: ['test/run_all.html'],
-      },
+    karma: {
       options: {
-        run: true
+        frameworks: ['mocha', 'chai'],
+        files: ['js/**/*.js', 'test/**/*.js'],
+        reporters: ['spec'],
+        plugins: [
+          'karma-phantomjs-launcher',
+          'karma-firefox-launcher',
+          'karma-chai',
+          'karma-mocha',
+          'karma-spec-reporter'
+        ]
+      },
+      unit: {
+        browsers: ['PhantomJS'],
+        singleRun: true
+      },
+      debug_unit: {
+        browsers: ['Firefox'],
+        client: {
+          mocha: {
+            reporter: 'html'
+          }
+        }
       }
     }
   });
 
-  // Load grunt mocha task
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('test', ['mocha']);
-
+  grunt.registerTask('test', ['karma:unit']);
+  grunt.registerTask('debug_test', ['karma:debug_unit']);
   grunt.registerTask('default', 'test');
 };
